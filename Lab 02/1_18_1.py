782c463866d226948d295ee405be33d99739750f
aryanp01441 <92912579+aryanp01441@users.noreply.github.com>
2021-10-21 02:27:15 -0500

    Add files via upload

---
+++ Lab 02/1_18_1.py
@@ -0,0 +1,4 @@
+# Type your code here
+print('Hello world!')
+print('How are you?')
+print('   (I\'m fine).')
---
+++ Lab 02/Lab02A.py
@@ -0,0 +1,8 @@
+PI = 3.14159
+
+radius = float(input('Enter the radius: '))
+height = float(input('Enter the height: '))
+
+surface_area = 2 * PI * radius * (radius + height)
+
+print('The surface area of the cylinder is', surface_area,'sqaure inches')
